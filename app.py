8737521b3f0ab95810376afcc895258334b8e014
Selena Deckelmann <selenamarie@gmail.com>
2013-01-21 11:03:33 -0800

    Fixed up syntax, refactored nesting mess Added --stop-after, cleaned up unused code Add better config parsing, also from a file Adding requirements list Create OPML class and add a timeout Beginnings of some flask support (but broken :/)

--- app.py
+++ app.py
@@ -1,9 +1,68 @@
-from flask import Flask
+
+import opml
+
+from flask import Flask,url_for,redirect,session,request
+from flask_oauth import OAuth
+
 app = Flask(__name__)
+
+oauth = OAuth()
+
+twitter = oauth.remote_app('twitter',
+    base_url='https://api.twitter.com/1/',
+    request_token_url='https://api.twitter.com/oauth/request_token',
+    access_token_url='https://api.twitter.com/oauth/access_token',
+    authorize_url='https://api.twitter.com/oauth/authenticate',
+    consumer_key='12Jbo5GoDxd6dq6epffIQ',
+    consumer_secret='00rIgEhKD944U3vt5vZdh0fXO4hEmQWY7vPOuyN7E',
+)
+
+#if not app.debug:
+    #import logging
+    #from themodule import TheHandlerYouWant
+    #file_handler = TheHandlerYouWant(...)
+    #file_handler.setLevel(logging.WARNING)
+    #app.logger.addHandler(file_handler)
+
+@app.route('/oauth-authorized')
+@twitter.authorized_handler
+def oauth_authorized(resp):
+    next_url = request.args.get('next') or url_for('index')
+    if resp is None:
+        flash(u'You denied the request to sign in.')
+        return redirect(next_url)
+
+    session['twitter_token'] = (
+        resp['oauth_token'],
+        resp['oauth_token_secret']
+    )
+    session['twitter_user'] = resp['screen_name']
+
+    flash('You were signed in as %s' % resp['screen_name'])
+    return redirect(next_url)
+
+@app.route('/login')
+def login():
+    return twitter.authorize(callback=url_for('oauth_authorized',
+        next=request.args.get('next') or request.referrer or None))
+
+@twitter.tokengetter
+def get_twitter_token(token=None):
+    return session.get('twitter_token')
+
+@app.route("/tweet")
+def status():
+    resp = twitter.get('statuses/home_timeline.json')
+    if resp.status == 200:
+        tweets = resp.data
+    else:
+        tweets = None
+        flash('Unable to load tweets from Twitter. Maybe out of '
+          'API calls or Twitter is overloaded.')
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
