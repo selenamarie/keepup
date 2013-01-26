eb92be5d85a21954c2366599a2a6b72149a6d911
Selena Deckelmann <selenamarie@gmail.com>
2013-01-26 12:28:23 -0800

    Adding upgrade script

--- db_migrate.py
+++ db_migrate.py
@@ -2,7 +2,7 @@
 
 import imp
 from migrate.versioning import api
-from app import db
+from keepup import db
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
 migration = SQLALCHEMY_MIGRATE_REPO + '/versions/%03d_migration.py' % (api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO) + 1)
