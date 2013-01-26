56994864f58280f3b6a5125083ed75c79fbaeab1
Selena Deckelmann <selenamarie@gmail.com>
2013-01-26 10:31:14 -0800

    Adding support for Flask-Login

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ import opml
 
 from flask import Flask,url_for,redirect,session,request
 from flask_oauth import OAuth
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.setup_app(app)
 
 oauth = OAuth()
 
